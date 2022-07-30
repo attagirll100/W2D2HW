13d2669cb51bd6a93d25bea62a769cbbd1fa1b85
Mary <attamary062@gmail.com>
2022-07-30 10:20:06 -0400

    homework2

---
+++ answers.js
@@ -0,0 +1,33 @@
+//Part 1
+//answer: 3
+
+
+
+for(let i=1; i<=20; i++);{
+    console.log('hi', i);
+}
+
+//Write a for loop that will log only the even numbers in 0 through 200.
+for(let i=0; i<200; i+=2); {
+    console.log(i);
+}
+
+
+//fizz buzz
+for(let i=1; i<=100; i++); {
+    if(i%3==1){
+        console.log("fizz")
+    }
+    else if(i%5==0){
+        console.log('buzz')
+    }
+    else if(i%3==0 && i%5 ==0) {
+        console.log("fizz buzz");
+    
+    }
+
+
+}
+
+
+
